6c30e1585a25ac6a829ff325bc46ba6d0d4ef440
pablomdd <pablo.mdd.007@gmail.com>
2021-05-05 13:19:54 -0500

    add comment to hello world assembler

--- linux-system-calls/helloWorld.s
+++ linux-system-calls/helloWorld.s
@@ -1,3 +1,10 @@
+@ --------
+@ swi: software interrupt. 
+@ The SWI instruction causes a SWI exception. 
+@ This means that the processor state changes to ARM, the processor mode changes to Supervisor, 
+@ the CPSR is saved to the Supervisor Mode SPSR, and execution branches to the SWI vector.
+@ -------
+
 .equ STDOUT,    1       @ #define STDOUT    1
 .equ MSG_LEN,   14      @ #define MSG_LEN   14
 .equ WRITE,     4
@@ -6,17 +13,21 @@
 LITERAL_MSG: .asciz "Hello, world!\n"
 
 .text
+@ save memory addres of character string 
 msg: .word   LITERAL_MSG     @ char *msg = "Hello, world!\n";
 
 .global main
 main:
 @    write(STDOUT, MSG, MSG_LEN);
+    @ save program state, ie, push relevant registries to the Call Stack
     push {r7, lr}
     mov r0, #STDOUT
     ldr r1, msg
     mov r2, #MSG_LEN
     mov r7, #WRITE
+    @ actual system call
     swi #0
+    @ pop from Call Stack
     pop {r7, lr}
 
 @   return 0
